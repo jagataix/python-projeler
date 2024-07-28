1c17dd172451bd3d651eb2fc2c6beda869a220da
jagataix <149905544+jagataix@users.noreply.github.com>
2024-07-29 02:15:19 +0300

    Add files via upload

---
+++ Python modüllerim/deneme.py
@@ -0,0 +1,9 @@
+import modul1
+
+print(modul1.topla(6,4,7))
+
+modul1.merhaba("Çağatay")
+
+print(modul1.programlama_dilleri)
+
+help(modul1)
---
+++ Python modüllerim/modul1.py
@@ -0,0 +1,14 @@
+programlama_dilleri = ["kanka","cay icek","gel hele","usta"]
+
+def topla(a,b,c):
+    """
+    Bu fonksiyon 3 degeri toplar
+    """
+
+    return a+b+c
+
+def merhaba(ad):
+    """
+    adınınz ile selam yazar
+    """
+    print("merhabalar",ad)
